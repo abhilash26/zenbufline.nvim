72b2aa14bf8a77b7b2add2ed6b40efdf014b5361
Abhilash Srivastava <abhilash@cuberoot.tech>
2024-07-13 09:42:21 +0530

    fix: format code

--- lua/zenbufline/config.lua
+++ lua/zenbufline/config.lua
@@ -1,31 +1,31 @@
 return {
-  line_start = "",
-  line_end = "",
-  modified = " [+] ",
-  hl = "ZenbuflineBuffer",
-  left = {
-    hl = "ZenbuflineNormal",
-    icon = "",
-  },
-  right = {
-    hl = "ZenbuflineNormal",
-    icon = ""
-  },
-  active = {
-    hl = "ZenbuflineActive",
-    italic = false,
-    bold = true,
-  },
-  inactive = {
-    hl = "ZenBuflineInactive",
-    italic = false,
-    bold = false,
-  },
-  exclude_fts = {
-    "neotree",
-    "NvimTree",
-    "Alpha",
-    "dashboard",
-    "help"
-  }
+	line_start = "",
+	line_end = "",
+	modified = " [+] ",
+	hl = "ZenbuflineBuffer",
+	left = {
+		hl = "ZenbuflineNormal",
+		icon = "",
+	},
+	right = {
+		hl = "ZenbuflineNormal",
+		icon = "",
+	},
+	active = {
+		hl = "ZenbuflineActive",
+		italic = false,
+		bold = true,
+	},
+	inactive = {
+		hl = "ZenBuflineInactive",
+		italic = false,
+		bold = false,
+	},
+	exclude_fts = {
+		"neotree",
+		"NvimTree",
+		"Alpha",
+		"dashboard",
+		"help",
+	},
 }
--- lua/zenbufline/init.lua
+++ lua/zenbufline/init.lua
@@ -3,93 +3,103 @@ local default_options = require("zenbufline.config")
 local o = default_options
 local cache = { left = "", right = "", active = "", inactive = "", bg = "" }
 local hls = {
-  ["ZenbuflineBuffer"] = "%#ZenbuflineBuffer#",
-  ["ZenbuflineNormal"] = "%#ZenbuflineNormal#",
-  ["ZenbuflineInactive"] = "%#ZenbuflineInactive#",
-  ["ZenbuflineActive"] = "%#ZenbuflineActive#",
+	["ZenbuflineBuffer"] = "%#ZenbuflineBuffer#",
+	["ZenbuflineNormal"] = "%#ZenbuflineNormal#",
+	["ZenbuflineInactive"] = "%#ZenbuflineInactive#",
+	["ZenbuflineActive"] = "%#ZenbuflineActive#",
 }
 
 -- Cache frequently used globals
 local api = vim.api
 
 local get_hl = function(hl)
-  return hls[hl] or string.format("%%#%s#", hl)
+	return hls[hl] or string.format("%%#%s#", hl)
 end
 
 M = {}
 
 M.define_highlights = function()
-  local status = api.nvim_get_hl(0, { name = "StatusLine" })
-  local normal = api.nvim_get_hl(0, { name = "Normal" })
-  local comment = api.nvim_get_hl(0, { name = "Comment" })
+	local status = api.nvim_get_hl(0, { name = "StatusLine" })
+	local normal = api.nvim_get_hl(0, { name = "Normal" })
+	local comment = api.nvim_get_hl(0, { name = "Comment" })
 
-  api.nvim_set_hl(0, "ZenbuflineBuffer", { link = "StatusLine" })
-  api.nvim_set_hl(0, "ZenbuflineNormal", { fg = normal.bg, bg = status.bg })
-  api.nvim_set_hl(0, "ZenbuflineInactive",
-    { fg = comment.fg, bg = normal.bg, bold = o.inactive.bold, italic = o.inactive.italic })
-  api.nvim_set_hl(0, "ZenbuflineActive",
-    { fg = normal.fg, bg = normal.bg, bold = o.active.bold, italic = o.active.italic })
+	api.nvim_set_hl(0, "ZenbuflineBuffer", { link = "StatusLine" })
+	api.nvim_set_hl(0, "ZenbuflineNormal", { fg = normal.bg, bg = status.bg })
+	api.nvim_set_hl(
+		0,
+		"ZenbuflineInactive",
+		{ fg = comment.fg, bg = normal.bg, bold = o.inactive.bold, italic = o.inactive.italic }
+	)
+	api.nvim_set_hl(
+		0,
+		"ZenbuflineActive",
+		{ fg = normal.fg, bg = normal.bg, bold = o.active.bold, italic = o.active.italic }
+	)
 end
 
 M.set_tabline = vim.schedule_wrap(function()
-  local line_parts = { cache["bg"] }
-  local cur_buf = api.nvim_get_current_buf()
-  local exclude_fts = o.exclude_fts
+	local line_parts = { cache["bg"] }
+	local cur_buf = api.nvim_get_current_buf()
+	local exclude_fts = o.exclude_fts
 
-  for _, buf in ipairs(api.nvim_list_bufs()) do
-    local bo = vim.bo[buf]
-    if api.nvim_buf_is_valid(buf) and bo.buflisted then
-      if not vim.tbl_contains(exclude_fts, bo.ft) then
-        local modified = bo.modified and o.modified or ""
-        local fname = vim.fn.fnamemodify(api.nvim_buf_get_name(buf), ":t");
-        line_parts[#line_parts + 1] = table.concat({
-          cache["left"],
-          cache[(buf == cur_buf) and "active" or "inactive"],
-          string.format(" %s%s ", fname, modified),
-          cache["right"],
-          cache["bg"]
-        }, "")
-      end
-    end
-  end
-  vim.o.tabline = table.concat(line_parts, " ")
+	for _, buf in ipairs(api.nvim_list_bufs()) do
+		local bo = vim.bo[buf]
+		if api.nvim_buf_is_valid(buf) and bo.buflisted then
+			if not vim.tbl_contains(exclude_fts, bo.ft) then
+				local modified = bo.modified and o.modified or ""
+				local fname = vim.fn.fnamemodify(api.nvim_buf_get_name(buf), ":t")
+				line_parts[#line_parts + 1] = table.concat({
+					cache["left"],
+					cache[(buf == cur_buf) and "active" or "inactive"],
+					string.format(" %s%s ", fname, modified),
+					cache["right"],
+					cache["bg"],
+				}, "")
+			end
+		end
+	end
+	vim.o.tabline = table.concat(line_parts, " ")
 end)
 
 M.cache_sections = function()
-  cache["left"] = string.format("%s%s", get_hl(o.left.hl), o.left.icon)
-  cache["right"] = string.format("%s%s", get_hl(o.right.hl), o.right.icon)
-  cache["active"] = get_hl(o.active.hl)
-  cache["inactive"] = get_hl(o.inactive.hl)
-  cache["bg"] = get_hl(o.hl)
+	cache["left"] = string.format("%s%s", get_hl(o.left.hl), o.left.icon)
+	cache["right"] = string.format("%s%s", get_hl(o.right.hl), o.right.icon)
+	cache["active"] = get_hl(o.active.hl)
+	cache["inactive"] = get_hl(o.inactive.hl)
+	cache["bg"] = get_hl(o.hl)
 end
 
 M.merge_config = function(opts)
-  -- prefer users config over default
-  o = vim.tbl_deep_extend("force", default_options, opts or {})
+	-- prefer users config over default
+	o = vim.tbl_deep_extend("force", default_options, opts or {})
 end
 
 M.create_autocommands = function()
-  local augroup = api.nvim_create_augroup("Zenbufline", { clear = true })
+	local augroup = api.nvim_create_augroup("Zenbufline", { clear = true })
 
-  -- create statusline
-  api.nvim_create_autocmd({ "BufEnter", "BufLeave", "BufDelete", "BufModifiedSet" }, {
-    group = augroup,
-    callback = M.set_tabline,
-    desc = "set tabline"
-  })
-  api.nvim_create_autocmd({ "ColorScheme" }, {
-    group = augroup,
-    callback = M.define_highlights,
-    desc = "update highlights"
-  })
+	-- create statusline
+	api.nvim_create_autocmd({ "BufEnter", "BufLeave", "BufDelete", "BufModifiedSet" }, {
+		group = augroup,
+		callback = M.set_tabline,
+		desc = "set tabline",
+	})
+	api.nvim_create_autocmd({ "ColorScheme" }, {
+		group = augroup,
+		callback = M.define_highlights,
+		desc = "update highlights",
+	})
 end
 
 M.setup = function(opts)
-  if plugin_loaded then return else plugin_loaded = true end
-  M.merge_config(opts)
-  M.create_autocommands()
-  M.define_highlights()
-  M.cache_sections()
+	if plugin_loaded then
+		return
+	else
+		plugin_loaded = true
+	end
+	M.merge_config(opts)
+	M.create_autocommands()
+	M.define_highlights()
+	M.cache_sections()
 end
 
 return M
